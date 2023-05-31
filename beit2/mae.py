14a994075812ab063061d3014307c0d14464d5b4
Yitzhak Levi <yitzhak.levi@mobileye.com>
2023-05-31 10:30:38 +0300

    revert mae

--- beit2/mae.py
+++ beit2/mae.py
@@ -13,7 +13,7 @@ def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
     out: (M, D)
     """
     assert embed_dim % 2 == 0
-    omega = np.arange(embed_dim // 2, dtype=np.float32)
+    omega = np.arange(embed_dim // 2, dtype=np.float)
     omega /= embed_dim / 2.
     omega = 1. / 10000 ** omega  # (D/2,)
 
@@ -55,7 +55,7 @@ def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
         pos_embed = np.concatenate([np.zeros([1, embed_dim]), pos_embed], axis=0)
     return pos_embed
 
-CodebookSize = 8192
+
 class MaskedAutoencoderViT(nn.Module):
     """ Masked Autoencoder with VisionTransformer backbone
     """
@@ -63,20 +63,19 @@ class MaskedAutoencoderViT(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3,
                  embed_dim=1024, depth=24, num_heads=16,
                  decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
-                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False, gammas_init_values=0.1, mask_ratio = 127/196.0):
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
         super().__init__()
 
         # --------------------------------------------------------------------------
         # MAE encoder specifics
-        self.patch_size = patch_size
         self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
         num_patches = self.patch_embed.num_patches
 
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
-        pos_embed = torch.zeros(1, num_patches, embed_dim)
+        pos_embed = torch.zeros(1, num_patches + 1, embed_dim)
         self.register_buffer('pos_embed', pos_embed)
         self.blocks = nn.ModuleList([
-            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer, init_values=gammas_init_values)
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer)
             for i in range(depth)])
         self.norm = norm_layer(embed_dim)
         # --------------------------------------------------------------------------
@@ -87,14 +86,14 @@ class MaskedAutoencoderViT(nn.Module):
 
         self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
 
-        decoder_pos_embed = torch.zeros(1, num_patches, decoder_embed_dim)
+        decoder_pos_embed = torch.zeros(1, num_patches + 1, decoder_embed_dim)
         self.register_buffer('decoder_pos_embed', decoder_pos_embed)
         self.decoder_blocks = nn.ModuleList([
-            Block(decoder_embed_dim, decoder_num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer, init_values=gammas_init_values)
+            Block(decoder_embed_dim, decoder_num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer)
             for i in range(decoder_depth)])
 
         self.decoder_norm = norm_layer(decoder_embed_dim)
-        self.decoder_pred = nn.Linear(decoder_embed_dim, CodebookSize, bias=True)  # decoder to beit
+        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size ** 2 * in_chans, bias=True)  # decoder to patch
         # --------------------------------------------------------------------------
 
         self.norm_pix_loss = norm_pix_loss
@@ -105,11 +104,11 @@ class MaskedAutoencoderViT(nn.Module):
         # initialization
         # initialize (and freeze) pos_embed by sin-cos embedding
         pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.patch_embed.num_patches ** .5),
-                                            cls_token=False)
+                                            cls_token=True)
         self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
 
         decoder_pos_embed = get_2d_sincos_pos_embed(self.decoder_pos_embed.shape[-1],
-                                                    int(self.patch_embed.num_patches ** .5), cls_token=False)
+                                                    int(self.patch_embed.num_patches ** .5), cls_token=True)
         self.decoder_pos_embed.data.copy_(torch.from_numpy(decoder_pos_embed).float().unsqueeze(0))
 
         # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
@@ -147,14 +146,60 @@ class MaskedAutoencoderViT(nn.Module):
         x = x.reshape(shape=(imgs.shape[0], h * w, p ** 2 * 3))
         return x
 
-    def forward_encoder(self, inputs, mask_ratio):
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        """
+        p = self.patch_embed.patch_size[0]
+        h = w = int(x.shape[1] ** .5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+        return imgs
+
+    def random_masking(self, x, mask_ratio):
+        """
+        Perform per-sample random masking by per-sample shuffling.
+        Per-sample shuffling is done by argsort random noise.
+        x: [N, L, D], sequence
+        """
+        N, L, D = x.shape  # batch, length, dim
+        len_keep = int(L * (1 - mask_ratio))
+
+        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+
+        # sort noise for each sample
+        ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+        # keep the first subset
+        ids_keep = ids_shuffle[:, :len_keep]
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+        # generate the binary mask: 0 is keep, 1 is remove
+        mask = torch.ones([N, L], device=x.device)
+        mask[:, :len_keep] = 0
+        # unshuffle to get the binary mask
+        mask = torch.gather(mask, dim=1, index=ids_restore)
+
+        return x_masked, mask, ids_restore
+
+    def forward_encoder(self, x, mask_ratio):
+        x = x['img']
         # embed patches
-        x = inputs['img']
         x = self.patch_embed(x)
-        x = inputs['one_hot'].to(x.dtype) @ x
+
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
+
+        # masking: length -> length * mask_ratio
+        x, mask, ids_restore = self.random_masking(x, mask_ratio)
 
         # append cls token
-        cls_token = self.cls_token
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
         cls_tokens = cls_token.expand(x.shape[0], -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
 
@@ -162,30 +207,21 @@ class MaskedAutoencoderViT(nn.Module):
         for blk in self.blocks:
             x = blk(x)
         x = self.norm(x)
-        return x, None, None
 
+        return x, mask, ids_restore
 
-    def forward_decoder(self, x, ids_restore, patch_idxs):
-        cls, emb = x[:, :1], x[:, 1:]
-        enc_cls = self.decoder_embed(cls)
-        enc_emb = self.decoder_embed(emb)
-        B, VIS, D = enc_emb.shape
-        PAD = self.patch_embed.num_patches - VIS
-        full_patches_shuffled = torch.cat([enc_emb, self.mask_token.expand([B, PAD, -1])], dim=1)
-        I = torch.argsort(patch_idxs, axis=-1).unsqueeze(-1).expand([-1, -1, D])
+    def forward_decoder(self, x, ids_restore):
+        # embed tokens
+        x = self.decoder_embed(x)
 
-        x = torch.concat([enc_cls, self.decoder_pos_embed + full_patches_shuffled.gather(dim=1, index=I)], dim = 1)
-        # # embed tokens
-        # x = self.decoder_embed(x)
-        #
-        # # append mask tokens to sequence
-        # mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
-        # x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
-        # x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
-        # x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
-        #
-        # # add pos embed
-        # x = x + self.decoder_pos_embed
+        # append mask tokens to sequence
+        mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
+        x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
+        x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
+        x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
+
+        # add pos embed
+        x = x + self.decoder_pos_embed
 
         # apply Transformer blocks
         for blk in self.decoder_blocks:
@@ -203,8 +239,10 @@ class MaskedAutoencoderViT(nn.Module):
 
     def forward(self, imgs, mask_ratio=0.75):
         latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio)
-        pred = self.forward_decoder(latent, ids_restore, imgs['all_idx'])  # [N, L, p*p*3]
+        pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
+        # outputs = {'pred': pred, 'mask': mask}
         return pred
+
 
 def mae_vit_base_patch16_dec512d8b(**kwargs):
     model = MaskedAutoencoderViT(
---
+++ beit2/main_fsdp.py
@@ -0,0 +1,151 @@
+import argparse
+import os
+import torch
+from torch.utils.data import DataLoader
+from beit2.beit_model import BeitTrainingModule
+from beit2.beit_local_dataset import BeitLocalDataset
+from beit2.beit_v2_loss import BeitV2Loss
+from beit2.distributed_utils import get_setup_defaults, initialize_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
+from itertools import chain
+from typing import Dict, Union
+import argparse
+HPU_AVAILABLE = True
+try:
+    import habana_frameworks.torch.core as htcore
+    import habana_frameworks.torch.hpu as hthpu
+    from habana_frameworks.torch.utils.library_loader import load_habana_module
+    from habana_frameworks.torch.hpex import hmp
+    import torch.distributed as dist
+except:
+    HPU_AVAILABLE = False
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, choices=['cpu', 'hpu'], required=True)
+    return parser.parse_args()
+
+
+patch_size = 16
+image_key = "img"
+img_shape = (224, 224)
+NH = img_shape[0]//patch_size
+NW = img_shape[1]//patch_size
+
+
+def enable_mixed_precision():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    bf16_file_path = os.path.join(dir_path, "ops_bf16.txt")
+    fp32_file_path = os.path.join(dir_path, "ops_fp32.txt")
+    hmp.convert(
+        opt_level="O1",
+        bf16_file_path=bf16_file_path,
+        fp32_file_path=fp32_file_path,
+        isVerbose=False,
+    )
+
+def get_model():
+    mae_model_config = {'patch_size': patch_size, 'in_chans': 1,
+                        'depth': 12, 'num_heads': 12, 'embed_dim': 768,
+                        'decoder_depth': 2, 'decoder_num_heads': 16, 'decoder_embed_dim': 512}
+    model = BeitTrainingModule(mae_model_config=mae_model_config)
+    return model
+
+
+def mp_fn(local_rank):
+    setup_args = get_setup_defaults(
+        local_rank=local_rank, processes_per_node=hthpu.device_count()
+    )
+    initialize_process_group(setup_args=setup_args, backend='hccl')
+    enable_mixed_precision()
+    device = torch.device("hpu")
+    model = get_model()
+    loss = BeitV2Loss()
+    optimizer = torch.optim.AdamW(params=model.parameters(), amsgrad=False)
+    num_params = sum(p.numel() for p in model.parameters())
+    print(f'built model with {num_params / 1e6}M params')
+    model = DDP(
+        model,
+        find_unused_parameters=True,
+        broadcast_buffers=False,
+        bucket_cap_mb=460,
+    )
+    batch_size = 128
+    dl = DataLoader(BeitLocalDataset(batch_size * 10000), batch_size=batch_size, num_workers=10)
+    import time
+    model.train()
+    log_interval = 10
+    t0 = time.perf_counter()
+
+    model.to(device)
+    loss.to(device)
+    for batch_idx, batch_data in enumerate(dl, start=1):
+        optimizer.zero_grad(set_to_none=True)
+
+        if isinstance(batch_data, dict):
+            for k, v in batch_data.items():
+                batch_data[k] = v.to(device)
+        else:
+            batch_data = batch_data.to(device)
+        with torch.autocast(device_type='hpu', enabled=True, dtype=torch.bfloat16):
+            outputs = model(batch_data)
+            l = loss(outputs, batch_data)['loss']
+
+        l.backward()
+        htcore.mark_step()
+        with hmp.disable_casts():
+            optimizer.step()
+            htcore.mark_step()
+
+        if batch_idx % log_interval == 0 and local_rank == 0:
+            time_passed = time.perf_counter() - t0
+            samples_processed = dist.get_world_size() * batch_size * log_interval
+            print(f'{samples_processed / time_passed} samples/second')
+            t0 = time.perf_counter()
+
+
+
+def train_on_cpu():
+    device = torch.device("cpu")
+    model = get_model()
+    loss = BeitV2Loss()
+    optimizer = torch.optim.AdamW(params=model.parameters(), amsgrad=False)
+    num_params = sum(p.numel() for p in model.parameters())
+    print(f'built model with {num_params / 1e6}M params')
+    batch_size = 128
+    dl = DataLoader(BeitLocalDataset(batch_size * 10000), batch_size=batch_size, num_workers=10)
+    import time
+    model.train()
+    log_interval = 10
+    t0 = time.perf_counter()
+
+    model.to(device)
+    loss.to(device)
+    for batch_idx, batch_data in enumerate(dl, start=1):
+        optimizer.zero_grad(set_to_none=True)
+
+        if isinstance(batch_data, dict):
+            for k, v in batch_data.items():
+                batch_data[k] = v.to(device)
+        else:
+            batch_data = batch_data.to(device)
+        outputs = model(batch_data)
+        l = loss(outputs, batch_data)['loss']
+        l.backward()
+
+
+        if batch_idx % log_interval:
+            time_passed = time.perf_counter() - t0
+            samples_processed = batch_size * log_interval
+            print(f'{samples_processed / time_passed} samples/second')
+            t0 = time.perf_counter()
+
+if __name__ == '__main__':
+    args = get_args()
+    if args.device == 'cpu':
+        train_on_cpu()
+    else:
+        assert (
+            hthpu.is_available() and HPU_AVAILABLE
+        ), "hpu training only supported on machines with habana gaudi accelerator"
+
